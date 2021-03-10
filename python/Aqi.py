7d0e653be00eb3a762c6864b6c2dcb32f6f5b2f7
mateo-manjarres <53178459+mateo-manjarres@users.noreply.github.com>
2021-03-10 02:30:42 -0500

    Update Aqi.py

--- python/Aqi.py
+++ python/Aqi.py
@@ -16,18 +16,16 @@ tam = len(customers_json.datos[1])
 
 
 def graficarHistorial(coordenadaX, coordenadaY):
-    #plt.figure('Historial Completo') 
+    
     for i in range(100):#cantidad de datos que se quiere
         m = []
         for j in range(21):
             m.append(customers_json.datos[j][i].get('valor'))
     
         m = np.array(m)
-        #fecha = customers_json.datos[1][i].get('fecha')
         grid_z2 = griddata((latitudes, longitudes), m, (coordenadaY, coordenadaX), method='cubic')
-        #plt.xticks(rotation=90)
-        if grid_z2 > 0 and grid_z2 < 150:
-            #plt.plot(fecha, grid_z2, 'r.', ms=5)
+        
+        if grid_z2 > 0 and grid_z2 < 300:
             print(calcularaqi(grid_z2))
         else:
             i=i-1
