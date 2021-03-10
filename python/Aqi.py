685ef601ac6e8db629b3f39abd49ae26dbeae1d0
mateo-manjarres <53178459+mateo-manjarres@users.noreply.github.com>
2021-03-10 02:01:05 -0500

    Update Aqi.py

--- python/Aqi.py
+++ python/Aqi.py
@@ -48,63 +48,7 @@ def graficarHistorial(coordenadaX, coordenadaY):
     #plt.title(titulo)
     #plt.show()
 
-# Punto 2
-def graficarFechaEnCoordenadaXY(coordenadaX, coordenadaY, dia, mes, anio, hora):
-    plt.figure('Coordenada X y Y en una Fecha') 
-    p = fechavscontaminacion(dia, mes, anio, hora)
-    m = []
-    for j in range(21):
-        m.append(customers_json.datos[j][p].get('valor'))
 
-    m = np.array(m)
-    fecha = customers_json.datos[1][p].get('fecha')
-    grid_z2 = griddata((latitudes, longitudes), m, (coordenadaY, coordenadaX), method='cubic')
-    plt.xticks(rotation=90)
-    if grid_z2 > 0 and grid_z2 < 1000:
-        plt.plot(fecha, grid_z2, 'r.', ms=5)
-    print(grid_z2)
-    titulo = 'Lon: ' + str(coordenadaX) + ' Lat: ' + str(coordenadaY)
-    plt.title(titulo)
-    plt.show()
 
-# Punto 5
-def animacion():
-    plt.figure('Animación') 
-    t=0
-    muestras = 50
-    m = []
-    f = []
-    grid_z0 =[]
-    grid_z2 =[]
-    
-    for i in range(muestras):
-        m = []
-        for j in range(21):
-            m.append(customers_json.datos[j][i].get('valor'))
-            
-        m = np.array(m)
-        f.append(customers_json.datos[1][i].get('fecha'))
-        grid_z0.append(griddata((latitudes, longitudes), m, (grid_y, grid_x), method='nearest'))
-        grid_z2.append(griddata((latitudes, longitudes), m, (grid_y, grid_x), method='cubic'))
-
-                    
-    while t < muestras:
-        for x in range(len(grid_z0)):
-            array = grid_z2[t]
-            array2 = grid_z0[t]
-            for y in range(len(grid_z0)):
-                if np.isnan(array[x,y]):
-                    array[x,y]=array2[x,y]
-        plt.contourf(grid_x, grid_y, grid_z2[t])
-        plt.title(f[t])
-        plt.show()
-        t = t+1
-        plt.pause(0.1)
-
-# Punto 1
-construirGrillaEnCiertaFecha('21', '02', '2020', '04')
-# Punto 2
 graficarHistorial(miX, miY) # Descomentar para obtener todo el historial (Se demora MUUUUCHOOOOO)
-graficarFechaEnCoordenadaXY(miX, miY, '21', '02', '2020', '04')
-# Punto 5
-animacion()
+
