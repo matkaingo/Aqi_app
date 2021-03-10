bb343fc559357821151b8ba56c21b6340173bd05
mateo-manjarres <53178459+mateo-manjarres@users.noreply.github.com>
2021-03-10 02:23:36 -0500

    Update Aqi.py

--- python/Aqi.py
+++ python/Aqi.py
@@ -28,12 +28,31 @@ def graficarHistorial(coordenadaX, coordenadaY):
         #plt.xticks(rotation=90)
         if grid_z2 > 0 and grid_z2 < 150:
             #plt.plot(fecha, grid_z2, 'r.', ms=5)
-            print(grid_z2)
+            print(calcularaqi(grid_z2))
         else:
             i=i-1
     #titulo = 'Lon: ' + str(coordenadaX) + ' Lat: ' + str(coordenadaY)
     #plt.title(titulo)
     #plt.show()
 
+def calcularaqi(pm25):
+    if pm25 > 0 and pm25 < 12:
+        return(((50 - 0)/(12-0))*(pm25-0)+0)
+        
+    elif pm25 > 12.1 and pm25 < 35.4:
+        return(((100 - 51)/(35.4-12.1))*(pm25-12.1)+51)
+        
+    elif pm25 > 35.5 and pm25 < 55.4:
+        return(((150 - 101)/(55.4-35.5))*(pm25-35.5)+101)
+        
+    elif pm25 > 55.5 and pm25 < 150.4:
+        return(((200 - 151)/(150.4-55.5))*(pm25-55.5)+151)
+        
+    elif pm25 > 150.5 and pm25 < 250.4:
+        return(((300 - 201)/(250.4-150.5))*(pm25-150.5)+201)
+        
+    elif pm25 > 250.5 and pm25 < 500.4:
+        return(((500 - 301)/(500-250.5))*(pm25-250.5)+301)
+    
 graficarHistorial(miX, miY) # Descomentar para obtener todo el historial (Se demora MUUUUCHOOOOO)
 
